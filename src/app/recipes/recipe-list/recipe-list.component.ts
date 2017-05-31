90af798e04b1aa4251de8325adaa03ad35fa2953
Michail Smirnov <michail.smirnov@activewindow.dk>
2017-05-31 19:26:04 +0300

    51 lessons comleted

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recepies = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Desription of a test recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-EwNAM6hqNfEA42JQSb0LZ416YQbVn38oqDtVltZC5cOkQWXt'),
+    new Recipe('A test recipe', 'Desription of a test recipe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-EwNAM6hqNfEA42JQSb0LZ416YQbVn38oqDtVltZC5cOkQWXt')
+  ];
   
   constructor() { }
 
